7725cd261ca3cb512fd6d1ea128bddc6d4552954
Thomas Salah <thomas.salah@gmail.com>
2021-02-07 15:50:45 +0000

    feat: fetch dino data + create dinos obj using class

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import {default as data} from './data/dino.json'
 import './styles/main.css'
 
 //form
@@ -11,3 +12,29 @@ const removeElm = (elm) => {
 button.addEventListener('click', (event) => {
   removeElm(event.target.parentNode)
 })
+
+// // Based class for human and dino
+class Animal {
+  constructor(diet, height, species, weight) {
+    this.diet = diet
+    this.height = height
+    this.species = species
+    this.weight = weight
+  }
+}
+//
+// //Dino Constructor Object
+class Dino extends Animal {
+  constructor({diet, fact, height, species, weight, when, where}) {
+    super(diet, height, species, weight)
+    this.fact = fact
+    this.when = when
+    this.where = where
+  }
+}
+
+//create Dino
+let dinos = []
+data.Dinos.forEach((elm) => dinos.push(new Dino(elm)))
+
+console.log('dinos', dinos)
