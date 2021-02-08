f0ba1ee89494c95409a4523a13d3ed5ceb666e2d
Thomas Salah <thomas.salah@gmail.com>
2021-02-08 09:16:36 +0000

    feat: compare dino fact

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import {default as data} from './data/dino.json'
 import './styles/main.css'
 
+let dinos = []
+let human = {}
+let gridContainer
 //form
 const button = document.getElementById('btn')
 const form = document.getElementById('dino-compare')
+const main = document.getElementById('main')
 
 //Remove an element and its parent from the DOM
 const removeElm = (elm) => {
   elm.parentNode.remove()
+  showGrid()
 }
 
 // Based class for human and dino
@@ -36,13 +41,12 @@ class Dino extends Animal {
       : [
           fact,
           `${species} could be found in ${where}`,
-          `${species} used tto exist from ${when}`,
+          `${species} used to exist from ${when}`,
         ]
   }
 }
 
 //create Dino
-let dinos = []
 data.Dinos.forEach((elm) => dinos.push(new Dino(elm)))
 
 //human
@@ -53,8 +57,6 @@ class Human extends Animal {
     this.name = name
   }
 }
-
-let human = {}
 const getUserData = () =>
   (function () {
     const humanData = {
@@ -67,11 +69,123 @@ const getUserData = () =>
     human = new Human(humanData)
   })()
 
+//Fact Comparison
+const compareFact = (dinos, human) => {
+  dinos.forEach((dino) => {
+    const weightFact = compareWeight(dino, Number(human.weight))
+    const dietFact = compareDiet(dino, human.diet.toLowerCase())
+    const heightFact = compareHeight(dino, human.height)
+    dino.fact =
+      dino.species === 'Pigeon'
+        ? dino.fact
+        : [...dino.fact, weightFact, dietFact, heightFact]
+  })
+}
+
+const compareWeight = (dino, humanWeight) =>
+  dino.weight > humanWeight
+    ? `you are ${dino.weight - humanWeight}lbs lighter than ${dino.species}`
+    : `you are ${humanWeight - dino.weight}lbs Heavier than ${dino.species}`
+
+const compareDiet = (dino, humanDiet) => {
+  let dietFact
+  if (dino.diet === humanDiet.toLowerCase()) {
+    dietFact = `you have the same ${dino.diet} diet than ${dino.species}`
+  } else if (dino.diet === 'herbavor') {
+    dietFact = `you are safe with ${dino.species}, I am on ${dino.diet} diet than`
+  } else if (dino.diet === 'carnivor') {
+    dietFact = `careful ${dino.species} has a ${dino.diet} diet, you can easily become its meal`
+  }
+  return dietFact
+}
+
+const compareHeight = (dino, humanHeight) =>
+  Number(dino.height) > humanHeight
+    ? `you are ${Number(dino.height) - humanHeight}inches smaller than ${
+        dino.species
+      }`
+    : `you are ${
+        parseInt(human.weight) - Number(dino.height)
+      }inches taller than ${dino.species}`
+
+// Layout
+const createTiles = (name, imgSrc, fact) => {
+  //grid-item
+  const tile = document.createElement('div')
+  tile.className = 'grid-item'
+
+  //Header
+  const header = document.createElement('h3')
+  const tileHeaderText = document.createTextNode(name)
+  header.appendChild(tileHeaderText)
+  tile.appendChild(header)
+
+  //image
+  const img = document.createElement('img')
+  img.src = imgSrc
+  tile.appendChild(img)
+
+  //Fact
+  if (fact) {
+    const textComponent = document.createElement('p')
+    const randomFact = fact[Math.floor(Math.random() * fact.length)]
+    const tileFact = document.createTextNode(randomFact)
+    textComponent.appendChild(tileFact)
+    tile.appendChild(textComponent)
+  }
+  return tile
+}
+
+const makeGrid = () => {
+  // Grid-container
+  gridContainer = document.createElement('div')
+  gridContainer.id = 'container'
+
+  // restartBtn
+  const restartBtn = document.createElement('button')
+  restartBtn.append('restart')
+  restartBtn.addEventListener('click', (event) => {
+    document.getElementById('grid').innerHTML = ''
+    dinos.splice(4, 1)
+    removeElm(event.target)
+  })
+  gridContainer.appendChild(restartBtn)
+
+  //grid
+  const grid = document.createElement('div')
+  grid.id = 'grid'
+  gridContainer.appendChild(grid)
+
+  // add human Obj to arr
+  dinos.splice(4, 0, human)
+  // Add tiles to DOM
+  dinos.forEach((item) => {
+    const tiles =
+      item.constructor.name === 'Dino'
+        ? createTiles(item.species, item.imgSrc, item.fact)
+        : createTiles(item.name, item.imgSrc)
+    grid.appendChild(tiles)
+  })
+}
+
+// Remove form from screen
+let isFormVisible = Boolean(form)
+
+const showGrid = () => {
+  if (isFormVisible) {
+    isFormVisible = false
+    main.appendChild(gridContainer)
+  } else {
+    isFormVisible = true
+    form.reset()
+    document.getElementById('main').appendChild(form)
+  }
+}
+
 //on form submit
 button.addEventListener('click', (event) => {
   getUserData()
-  console.log('human', human)
+  compareFact(dinos, human)
+  makeGrid()
   removeElm(event.target.parentNode)
 })
-
-console.log('dinos', dinos)
